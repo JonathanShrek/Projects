b6a5a75b35d5997265e7575c16f45345d32fe92e
Jonathan Shreckengost <jonathanshrek@gmail.com>
2018-12-22 12:41:09 -0500

    Fixed else statement so username now gets written to txt file.

--- userLogin.py
+++ userLogin.py
@@ -6,26 +6,26 @@ def newUser():
     print('Confirm password: ')
     confirmUserPass = input("")
 
-#FIXME error in else statement
     if newUserPass != confirmUserPass:
         for i in range(3, -1, -1):
             if newUserPass != confirmUserPass:
                 print(i, 'chances remain.')
                 print('Error: Confirm password:')
                 confirmUserPass = input("")
-            else:
-                f = open('accounts.txt', 'a+')
-                f.writelines([newUserName,'\n'])
-                f.close()
-                print('Success')
-                print('Enter username: ')
-                break
+    else:
+        with open('accounts.txt', 'a') as out:
+            out.write(newUserName + '\n')
+            out.close()
+        print('Success')
+        print('Enter username: ')
 
+#FIXME does not read line to check if username exists
 def findUser(n):
     f = open('accounts.txt', 'r')
     name = n
     while True:
         text = f.readlines()
+        f.close()
         for line in text:
             if name in text:
                 print('Success')

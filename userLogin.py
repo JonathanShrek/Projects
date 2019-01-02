b1379171e48b9e13305e991c2b9e3685d1b2beb3
Jonathan Shreckengost <jonathanshrek@gmail.com>
2019-01-01 20:39:53 -0500

    Hashed user passwords into blake2 hash. Also organized functions into classes.

--- userLogin.py
+++ userLogin.py
@@ -1,56 +1,76 @@
-def fileRead():
-    with open('accounts.txt') as f:
-        lines = f.read().splitlines()
-        return lines
+class password:
+    def hash_password(self, password):
+        from hashlib import blake2b
+        h = blake2b(digest_size=64)
+        passwordByte = str.encode(password)
+        h.update(passwordByte)
+        return h.hexdigest()
 
-def fileWrite(name, password):
-    with open('accounts.txt', 'a') as out:
-        out.write(name + ' ' + password + '\n')
-        out.close()
-
-def newUser():
-    newUserName = input("Enter a new username: ")
-    newUserPass = input("Enter a new user password: ")
-    confirmUserPass = input("Confirm password: ")
-
-    for i in range(3, -1, -1):
-        if newUserPass == confirmUserPass:
-            fileWrite(newUserName, newUserPass)
-            print("Success.")
-            print("Enter username: ")
-            break
-
+    def passwordFunc(self, p):
+        read = fileManipulation()
+        passClass = password()
+        namePassList = read.fileRead()
+        passList = [i.split(' ')[1] for i in namePassList]
+        passHash = passClass.hash_password(p)
+        if passHash in passList:
+            return True
         else:
-            if i == 0:
-                print("Error: Max attempts reached.")
-                print('')
-                print("Enter username:")
-                print("Enter new to create a new user")
+            return False
+
+class fileManipulation:
+    def fileRead(self):
+        with open('accounts.txt') as f:
+            lines = f.read().splitlines()
+            return lines
+
+    def fileWrite(self, name, password):
+        with open('accounts.txt', 'a') as out:
+            out.write(name + ' ' + password + '\n')
+            out.close()
+
+class user:
+    def newUser(self):
+        passClass = password()
+        fileClass = fileManipulation()
+        newUserName = input("Enter a new username: ")
+        newUserPass = input("Enter a new user password: ")
+        confirmUserPass = input("Confirm password: ")
+
+        for i in range(3, -1, -1):
+            if newUserPass == confirmUserPass:
+                passHash = passClass.hash_password(newUserPass)
+                fileClass.fileWrite(newUserName, passHash)
+                print("Success.")
+                print("Enter username: ")
                 break
 
             else:
-                print('')
-                print("Password confirmation unsuccessful.")
-                print(i, "attempts remaining.")
-                confirmUserPass = input("Confirm password: ")
+                if i == 0:
+                    print("Error: Max attempts reached.")
+                    print('')
+                    print("Enter username:")
+                    print("Enter new to create a new user")
+                    break
 
-def findUser(n):
-    namePassList = fileRead()
-    nameList = [i.split(' ')[0] for i in namePassList]
-    if n in nameList:
-        return True
-    else:
-        return False
+                else:
+                    print('')
+                    print("Password confirmation unsuccessful.")
+                    print(i, "attempts remaining.")
+                    confirmUserPass = input("Confirm password: ")
 
-def passwordFunc(p):
-    namePassList = fileRead()
-    passList = [i.split(' ')[1] for i in namePassList]
-    if p in passList:
-        return True
-    else:
-        return False
+    def findUser(self, n):
+        fileClass = fileManipulation()
+        namePassList = fileClass.fileRead()
+        nameList = [i.split(' ')[0] for i in namePassList]
+        if n in nameList:
+            return True
+        else:
+            return False
+
 
 def main():
+    userClass = user()
+    passClass = password()
     print("Enter new to create a new user")
     print("Enter username: ")
     while True:
@@ -61,14 +81,14 @@ def main():
             break
 
         elif userinput == 'new' or userinput == 'New':
-            newUser() 
+            userClass.newUser() 
 
         else:
-            if findUser(userinput) == True:
-                password = input("Enter password: ")
+            if userClass.findUser(userinput) == True:
+                passInput = input("Enter password: ")
 
                 for i in range(3, -1, -1):
-                    if passwordFunc(password) == True:
+                    if passClass.passwordFunc(passInput) == True:
                         print("Successfully logged in. Welcome", userinput + "!")
                         break
 
@@ -81,7 +101,7 @@ def main():
                             break
                         else:
                             print("Incorrect password.", i, "attempts remain.")
-                            password = input("Enter password: ")
+                            passInput = input("Enter password: ")
 
             else:
                 print("Error: Unknown username.")
